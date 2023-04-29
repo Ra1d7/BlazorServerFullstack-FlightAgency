2f0e3b3277be9ecacbb7b877a54fe364c4730f89
Raid7 Raid7 <thmrgb8@gmail.com>
2023-04-29 16:51:04 +0300

    Fixed getting values by making the contact form work

---
+++ Flight/Pages/Contact.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
+
+namespace Flight.Pages
+{
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [IgnoreAntiforgeryToken]
+    public class ContactModel : PageModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        private readonly ILogger<ContactModel> _logger;
+
+        public void OnPost()
+        {
+            var fullname = Request.Form["fullname"];
+            var email = Request.Form["email"];
+            var phone = Request.Form["phone"];
+            var message = Request.Form["message"];
+
+            ViewData["confirm"] = $"thank you {fullname}, we will forward your message";
+
+        }
+
+        public ContactModel(ILogger<ContactModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        }
+    }
+}
